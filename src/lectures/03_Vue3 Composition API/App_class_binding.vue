63936a001c5b19ea0c793eefa4ff910bd3fa287f
Chaewon <fromnowwon@gmail.com>
2023-11-21 16:06:27 +0900

    03_Vue3 Composition API_06_Class와 Style 바인딩

---
+++ src/lectures/03_Vue3 Composition API/App_class_binding.vue
@@ -0,0 +1,48 @@
+<template>
+	<div>
+		<div class="text" :class="classObject">텍스트입니다.</div>
+		<button v-on:click="toggle">Toggle</button>
+		<button v-on:click="hasError = !hasError">Toggle Error</button>
+	</div>
+</template>
+
+<script>
+import { ref, computed } from 'vue';
+
+export default {
+	setup() {
+		const isActive = ref(true);
+		const hasError = ref(false);
+		// const classObject = {
+		// 	active: true,
+		// 	'text-danger': true,
+		// };
+		const classObject = computed(() => {
+			return {
+				active: true,
+				'text-danger': true,
+			};
+		});
+
+		const toggle = () => {
+			isActive.value = !isActive.value;
+		};
+
+		return {
+			isActive,
+			toggle,
+			hasError,
+			classObject,
+		};
+	},
+};
+</script>
+
+<style scoped>
+.active {
+	font-weight: 900;
+}
+.text-danger {
+	color: red;
+}
+</style>
---
+++ src/lectures/03_Vue3 Composition API/App_style_binding.vue
@@ -0,0 +1,38 @@
+<template>
+	<div :style="styleObject">
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. In, nemo
+		suscipit delectus totam pariatur commodi eaque voluptas voluptate ad
+		architecto officia. Ab facere voluptas saepe deleniti blanditiis
+		accusantium consequuntur iste!
+	</div>
+	<button v-on:click="fontSize--">-</button>
+	<button v-on:click="fontSize++">+</button>
+</template>
+
+<script>
+import { computed, ref } from 'vue';
+export default {
+	setup() {
+		// const styleObject = reactive({
+		// 	color: 'red',
+		// 	fontSize: '13px',
+		// });
+
+		const fontSize = ref(13);
+
+		const styleObject = computed(() => {
+			return {
+				color: 'red',
+				fontSize: fontSize.value + 'px',
+			};
+		});
+
+		return {
+			styleObject,
+			fontSize,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
