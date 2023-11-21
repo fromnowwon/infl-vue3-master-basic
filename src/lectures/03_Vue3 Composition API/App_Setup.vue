085a767825234132ef875bec8d5f01bb12c13e40
Chaewon <fromnowwon@gmail.com>
2023-11-21 14:03:34 +0900

    03_Vue3 Composition API_02_Setup 함수

---
+++ src/lectures/03_Vue3 Composition API/App_Setup.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		{{ counter }}
+	</div>
+	<button @click="increment">Click!</button>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+	setup() {
+		const counter = ref(0);
+		const increment = () => {
+			counter.value++;
+		};
+
+		return {
+			counter,
+			increment,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
