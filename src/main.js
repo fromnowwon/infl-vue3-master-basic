668baf4cc969b006b90b9cd2087efa57e35ad5f5
Chaewon <fromnowwon@gmail.com>
2023-12-28 16:26:27 +0900

    05_컴포넌트 기초+심화_01_컴포넌트 기초

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+// 전역 컴포넌트
+// app.component('AppCard', AppCard);
+
+app.mount('#app');
 
 import 'bootstrap/dist/js/bootstrap.js';
